705bda08bdbe0c45cde5c8e260817565331c3d02
Your Name <lubomir.anastasov@gmail.com>
2010-10-03 20:06:00 +0100

    solution1 first modification

--- kata2_solution1.rb
+++ kata2_solution1.rb
@@ -1,24 +1,24 @@
-arr = [ 1,12, 33, 45, 75,86, 97,108, 900 ]
-arr0= [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+arr = [1,12, 33, 45, 75,86, 97,108, 900]
+arr0 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-@left = 0
-@right = arr.length
-def bsearch( num, arr )
-  @mid = (@left + @right)/2
-  while num != arr[@mid] do
-    if num < arr[@mid] then
-      @right = @mid
-      @mid = (@left + @right)/2
-    elsif num > arr[@mid] then
-      @left = @mid
-      @mid = (@left + @right)/2
+def bsearch(num, arr, left, right)
+  mid = (left + right)/2
+  while num != arr[mid] 
+    if num < arr[mid] 
+      right = mid
+      mid = (left + right)/2
+    elsif num > arr[mid] 
+      left = mid
+      mid = (left + right)/2
     end
-    
   end
-  if num == arr[@mid] : puts @mid
-  end
-  yield
+  puts mid if num == arr[mid]
 end
 
-@seek=900
-bsearch( @seek, arr ){puts "arr[#{@mid}]=#{@seek}\n" }
+1.upto(9) do |seek|
+	bsearch(seek, arr0, 0, arr0.length) 
+end
+
+for seek1 in arr 
+		bsearch(seek1, arr, 0, arr.length)
+end
