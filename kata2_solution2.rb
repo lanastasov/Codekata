6115eff2e6a94202a5456a24b0dd922444be8b8b
Your Name <lubomir.anastasov@gmail.com>
2010-10-03 20:36:20 +0100

    solution2 first modification
    Recursive Solution

--- kata2_solution2.rb
+++ kata2_solution2.rb
@@ -1,19 +1,19 @@
-mas = [1, 2, 3, 4, 5]
-arr = [4, 8, 19, 20, 21, 45 ]
-def binsearch(mas,a,l,r)
-  if l > r : return 0
-	end
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+arr0 = [4, 8, 19, 20, 21, 45, 55]
 
-	if a == mas[(l+r)/2] 
-			return (l+r)/2;
-	elsif a < mas[(l+r)/2] 
-		return binsearch(mas,a,l,(l+r)/2)
-	elsif a > mas[(l+r)/2]
-	  return binsearch(mas,a,(l+r)/2,r)
+def binsearch(mas, num, l, r)
+  return 0 if l > r
+	if num == mas[(l+r)/2] 
+		return (l+r)/2;
+	elsif num < mas[(l+r)/2] 
+		return binsearch(mas, num, l, (l+r)/2)
+	elsif num > mas[(l+r)/2]
+	  return binsearch(mas, num, (l+r)/2, r)
 	end
 end
 
-#puts mas
-puts binsearch(arr,20,0,5)
+for i in arr
+	puts binsearch(arr, i, 0, arr.length)
+end
 
   
