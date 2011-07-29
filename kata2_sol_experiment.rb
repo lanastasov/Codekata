9788b21117c12ede008914636d005e5a604b6436
lanastasov <lubomir.anastasov@gmail.com>
2011-07-30 00:25:26 +0300

    remember some git skillz

--- kata2_sol_experiment.rb
+++ kata2_sol_experiment.rb
@@ -1,8 +1,8 @@
 #require 'test/unit'
-a = [ 1, 2, 3, 4, 5, 6 ]
-g = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ]
+a = [1, 2, 3, 4, 5, 6]
+g = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
   @position = g.length/2
-def bsearch( num, mas )
+def bsearch(num, mas)
   if mas[mas.length/2] == num then
     return @position  
   elsif num < mas[mas.length/2] then
@@ -15,7 +15,7 @@ def bsearch( num, mas )
     (mas.length/2 + 1).times do
       mas.delete_at(@pos)
     end
-      bsearch( num, mas)
+      bsearch(num, mas)
   elsif num > mas[mas.length/2] then
 #   @old_postion = @position
     @position = @position + (mas.length/4 + 1 ) 
@@ -23,11 +23,11 @@ def bsearch( num, mas )
     (mas.length/2 + 1).times do
       mas.delete_at(0)
     end
-    bsearch( num, mas )
+    bsearch(num, mas)
     
   end
 end
-puts bsearch( 5, g )
+puts bsearch(5, g) 
 
 #Doesn't work! Why?
 #1.upto(11) do |x|
