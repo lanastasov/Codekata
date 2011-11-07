d62a130fb0dc66efec7536ea8013d838885973c6
lanastasov <lubomir.anastasov@gmail.com>
2011-11-07 16:02:30 +0200

    Update timer_ruby.rb

--- timer_ruby.rb
+++ timer_ruby.rb
@@ -12,37 +12,37 @@ time_vatiable = Time.local(2011,"Nov",5,07,00,00)
 
 #ttime.ctime
 def output_time_units(ttime)
-	day = ttime.day
-	hour = ttime.hour
-	minute = ttime.min
-	while day == ttime.day
-		print "Study: ", ttime.hour,":#{ttime.min <= 9 ? "0" : ""}", ttime.min, " -->  " if ((ttime + $base_studing_unit).day == ttime.day) 
-		ttime += $base_studing_unit
-		$for_every_four_base_studing_units_one_medium_break_unit +=1
-		$total_hours_studied += 1
-		break if day != ttime.day
-		if ttime.min > 9 
-			print ttime.hour,":",ttime.min,"\n"
-		else
-			print ttime.hour,":0",ttime.min,"\n"
-		end
+  day = ttime.day
+  hour = ttime.hour
+  minute = ttime.min
+  while day == ttime.day
+    print "Study: ", ttime.hour,":#{ttime.min <= 9 ? "0" : ""}", ttime.min, " -->  " if ((ttime + $base_studing_unit).day == ttime.day) 
+    ttime += $base_studing_unit
+    $for_every_four_base_studing_units_one_medium_break_unit +=1
+    $total_hours_studied += 1
+    break if day != ttime.day
+    if ttime.min > 9 
+      print ttime.hour,":",ttime.min,"\n"
+    else
+      print ttime.hour,":0",ttime.min,"\n"
+    end
 		
-		case $for_every_four_base_studing_units_one_medium_break_unit
-			when 4
-				print "Break: #{$medium_break_unit/60}	", ttime.hour,":#{ttime.min <= 9 ? "0" : ""}",ttime.min, " -->  "
-				ttime	+= $medium_break_unit
-			when 8
-				print "Break: #{$long_break_unit/60}	", ttime.hour,":#{ttime.min <= 9 ? "0" : ""}",ttime.min, " -->  "
-				ttime += $long_break_unit
-				$for_every_four_base_studing_units_one_medium_break_unit = 0
-			else
-				print "Break: #{$base_break_unit/60}	", ttime.hour,":#{ttime.min <= 9 ? "0" : ""}",ttime.min, " -->  "
-				ttime += $base_break_unit
-		end
-		break if day != ttime.day
-		print ttime.hour,":#{ttime.min <= 9 ? "0" : ""}",ttime.min,"\n"
+    case $for_every_four_base_studing_units_one_medium_break_unit
+    when 4
+      print "Break: #{$medium_break_unit/60}	", ttime.hour,":#{ttime.min <= 9 ? "0" : ""}",ttime.min, " -->  "
+      ttime	+= $medium_break_unit
+    when 8
+      print "Break: #{$long_break_unit/60}	", ttime.hour,":#{ttime.min <= 9 ? "0" : ""}",ttime.min, " -->  "
+      ttime += $long_break_unit
+      $for_every_four_base_studing_units_one_medium_break_unit = 0
+    else
+      print "Break: #{$base_break_unit/60}	", ttime.hour,":#{ttime.min <= 9 ? "0" : ""}",ttime.min, " -->  "
+      ttime += $base_break_unit
+    end
+    break if day != ttime.day
+    print ttime.hour,":#{ttime.min <= 9 ? "0" : ""}",ttime.min,"\n"
 		
-	end
+  end
 end
 
 output_time_units(time_vatiable)
