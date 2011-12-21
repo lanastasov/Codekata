84c5ae4492f36931205780f33a871af017085452
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2011-12-22 01:31:30 +0200

    String reverse

---
+++ rev.rb
@@ -0,0 +1,18 @@
+!def rev(str)
+  loop = str.length
+  word = ""
+  while loop > 0
+	word << str[loop-=1]
+  end
+  return word;
+end
+
+def rev2(str)
+  i = str.length
+  word = ""
+  str.length.times {|x| word.insert(x, str[-x-1])}
+  return word
+end
+
+puts rev("foobar")
+puts rev2("foobar")
