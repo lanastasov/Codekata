4d9d8221d1a29b8bb443b093adb13156846f3756
lanastasov <lubomir.anastasov@gmail.com>
2012-09-19 06:10:45 +0300

    utf-8 so that cyrillic text displayed correctly

--- kata2_solution2.rb
+++ kata2_solution2.rb
@@ -1,19 +1,19 @@
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+﻿arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 arr0 = [4, 8, 19, 20, 21, 45, 55]
 
 def binsearch(mas, num, l, r)
   return 0 if l > r
-	if num == mas[(l+r)/2] 
-		return (l+r)/2;
-	elsif num < mas[(l+r)/2] 
-		return binsearch(mas, num, l, (l+r)/2)
-	elsif num > mas[(l+r)/2]
-	  return binsearch(mas, num, (l+r)/2, r)
-	end
+  if num == mas[(l+r)/2] 
+	return (l+r)/2;
+  elsif num < mas[(l+r)/2] 
+	return binsearch(mas, num, l, (l+r)/2)
+  elsif num > mas[(l+r)/2]
+	return binsearch(mas, num, (l+r)/2, r)
+  end
 end
 
 for i in arr
-	puts binsearch(arr, i, 0, arr.length)
+  puts binsearch(arr, i, 0, arr.length)
 end
 
 
