f7d34423c1b52734cc0669348f9a823e6e95d601
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2015-09-25 16:19:54 +0300

    wmv-mp4

---
+++ wmv-to-mp4/wmv_to_mp4.rb
@@ -0,0 +1,25 @@
+# directory of wmv files
+wmv_files_path = "E:/lanastasov/HASKELL/Haskell Fundamentals Part 1"
+
+if wmv_files_path == "" then
+	wmv_files_path = Dir.pwd
+else
+	Dir.chdir ("#{wmv_files_path}")
+end
+
+dirs = []
+Dir.glob("*") { |x|
+	dirs << x if x != "\n" && File.directory?("#{x}") 
+}
+
+if dirs.length > 0 then
+	for dir in dirs do 
+		Dir.chdir("#{dir}")
+		Dir.glob("*").each { |file|
+			if file =~ /.wmv/ then
+				system("ffmpeg -i \"#{file}\" \"#{file.gsub(".wmv", ".mp4")}\"")	
+			end
+		}
+		Dir.chdir("..")
+	end
+end
