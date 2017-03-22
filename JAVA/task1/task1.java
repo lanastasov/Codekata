31941d4e39c1da9a5bcaef53ef73aeacaf384d0b
lanastasov <lubomir.anastasov@gmail.com>
2017-03-22 13:07:01 +0200

    Update task1.java

--- JAVA/task1/task1.java
+++ JAVA/task1/task1.java
@@ -6,22 +6,31 @@ public class ReverseString {
 	
 	static String RevString2(String str) {
 		char[] in = str.toCharArray();
-	    int begin=0;
-	    int end=in.length-1;
-	    char temp;
-	    while(end>begin){
-	        temp = in[begin];
-	        in[begin]=in[end];
-	        in[end] = temp;
-	        end--;
-	        begin++;
-	    }
-	    return new String(in);
+		int begin=0;
+		int end=in.length-1;
+		char temp;
+		while(end>begin){
+			temp = in[begin];
+			in[begin]=in[end];
+			in[end] = temp;
+			end--;
+			begin++;
+		}
+		return new String(in);
+	}
+	
+	static String RevString3(String str) {
+		char[] rev = str.toCharArray();
+		String revString = "";
+		for (int i = rev.length-1; i>=0; i--) 
+			revString += rev[i];
+		return revString;
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("String after reverse: "+ RevString1("foobar1"));
 		System.out.println("String after reverse: "+ RevString2("foobar2"));
+		System.out.println("String after reverse: "+ RevString3("foobar3"));
 	}
 
 }
