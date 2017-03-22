a29c5a4fce155dc1818eb9c613172528dfab5d8e
lanastasov <lubomir.anastasov@gmail.com>
2017-03-22 16:41:05 +0200

    Create day2_Operators.java

---
+++ JAVA/hr-30days/day2_Operators.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.math.*;
+
+public class Day2_Operators {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+      
+        // Write your calculation code here.
+        mealCost = mealCost + mealCost*100/tipPercent + mealCost*100/taxPercent;
+        // cast the result of the rounding operation to an int and save it as totalCost 
+        int totalCost = (int) Math.round(mealCost);
+      
+        // Print your result
+        System.out.println(totalCost);
+    }
+}
