1719e4844fe8190df92e549c0089a972cd660b56
lanastasov <lubomir.anastasov@gmail.com>
2017-03-23 14:56:03 +0200

    Update Day7_Arrays.java

--- JAVA/hr-30days/Day7_Arrays.java
+++ JAVA/hr-30days/Day7_Arrays.java
@@ -4,15 +4,15 @@ import java.util.*;
 public class Day7_Arrays {
 
 	public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] arr = new int[n];
-        for(int i=0; i < n; i++){
-            arr[i] = in.nextInt();
-        }
-        in.close();
-        for(int j=n-1; j >= 0; j--) {
-            System.out.printf("%d ",arr[j]);
-        }
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] arr = new int[n];
+		for(int i=0; i < n; i++){
+		    arr[i] = in.nextInt();
+		}
+		in.close();
+		for(int j=n-1; j >= 0; j--) {
+		    System.out.printf("%d ",arr[j]);
+		}
 	}
 }
