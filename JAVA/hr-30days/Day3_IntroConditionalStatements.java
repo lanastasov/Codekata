10b1ea655488097ffcaaa0e32bfecf78372a7bda
lanastasov <lubomir.anastasov@gmail.com>
2017-03-23 13:19:04 +0200

    Update and rename day3_IntroConditionalStatements.java to Day3_IntroConditionalStatements.java

--- JAVA/hr-30days/Day3_IntroConditionalStatements.java
+++ JAVA/hr-30days/Day3_IntroConditionalStatements.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class day3_IntroConditionalStatements {
+public class Day3_IntroConditionalStatements {
 
 	public static void main(String[] args) {
 		  Scanner scan = new Scanner(System.in);
@@ -13,8 +13,8 @@ public class day3_IntroConditionalStatements {
 		  String ans="";
 
 		  // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
-		  if(n%2==1){
-		     ans = "Weird";
+		  if(n%2 == 1){
+	     		ans = "Weird";
 		  }
 		  else{
 			if (n >= 2 && n <= 5) 
