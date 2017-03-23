f0082d44b6ea3406b3e879a8f47ed49855dce2ad
lanastasov <lubomir.anastasov@gmail.com>
2017-03-23 14:20:28 +0200

    Update Day6_Review.java

--- JAVA/hr-30days/Day6_Review.java
+++ JAVA/hr-30days/Day6_Review.java
@@ -6,7 +6,7 @@ public class Day6_Review {
 		char[] rev = str.toCharArray();
 		String ans="";
 		for (int i=0; i<rev.length; i+=2) {
-      ans += rev[i];
+      			ans += rev[i];
 		}
 		ans += " ";
 		for (int j=1; j<rev.length; j+=2) {
@@ -17,16 +17,17 @@ public class Day6_Review {
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        in.nextLine();
-        String[] ss = new String[n];
-        for (int i=0; i<n; i++) {
-        	 ss[i] = in.nextLine();
-        }
-        
-        for( String element:ss) {
-        	System.out.println(Review(element));
-        }
+		int n = in.nextInt();
+		in.nextLine(); // read empty string
+		String[] ss = new String[n];
+		
+		for( int i=0; i<n; i++ ) {
+			 ss[i] = in.nextLine();
+		}
+
+		for( String element:ss ) {
+			System.out.println(Review(element));
+		}
 	}	
 
 }
